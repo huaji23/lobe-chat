95ee5a5e78f7f373ee8c76cbea86eee25e37b0ee
nobug <376330019@qq.com>
2024-10-01 00:25:46 -0300

    你的提交信息

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -5,6 +5,6 @@
 export const LOBE_CHAT_CLOUD = '聪明才基 Cloud';
 
 export const BRANDING_NAME = '聪明才基';
-export const BRANDING_LOGO_URL = 'https://movie.jihuiduoduo.top/img_yellow31.svg';
+export const BRANDING_LOGO_URL = 'https://movie.jihuiduoduo.top/小鸡1.svg';
 
 export const ORG_NAME = '聪明才基';
